767a6cf2f9c66dc4faddecf3bb51b4fc3f16370c
hnim <minh.nguyen.intern@mohasoftware.com>
2025-02-26 17:28:12 +0700

    update clock

--- src/components/Tab.vue
+++ src/components/Tab.vue
@@ -19,10 +19,6 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/></svg>
       </button>
     </div>
-
-    <div class="tab-content">
-      <router-view />
-    </div>
   </div>
 </template>
 
@@ -60,7 +56,6 @@ const goHome = () => router.push("/");
 .tabs-header {
   width: 100%;
   background-color: #ffffff;
-  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
 }
 
 /* 🌟 Tabs */
