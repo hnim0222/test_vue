86b45f71b55f44429f2559554a24348b0ca242dc
hnim <83656867+hnim0222@users.noreply.github.com>
2025-08-15 04:10:31 +0000

    update height sopitfy

--- src/components/Spotify.vue
+++ src/components/Spotify.vue
@@ -1,5 +1,5 @@
 <template>
   <div style="padding: 0 20px;">
-    <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/0AC4cozFjWzPY1McmjQzWW?utm_source=generator" width="100%" height="352" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/0AC4cozFjWzPY1McmjQzWW?utm_source=generator" width="100%" height="850px" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
   </div>
 </template>
